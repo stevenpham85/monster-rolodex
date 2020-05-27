79c2243e0ee74187bf39ecf48e32d780ce262a97
Steven Pham <stevenpham.may@gmail.com>
2020-05-27 10:23:51 -0400

    Updates

---
+++ precache-manifest.1a5a3b81444b8210ed2cd739c9108ea8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5515b7366905c04e62149cda417064ca",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "73643b3dd175abdda789",
+    "url": "/monster-rolodex/static/css/main.37607756.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monster-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73643b3dd175abdda789",
+    "url": "/monster-rolodex/static/js/main.9b2a6640.chunk.js"
+  },
+  {
+    "revision": "8575afebb5261fef1571",
+    "url": "/monster-rolodex/static/js/runtime-main.502e2e68.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monster-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
